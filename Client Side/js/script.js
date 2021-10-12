15650db51e0f4945e22761196b5aa2116889f38a
Shohrat-Code <shohrat@code.edu.az>
2021-10-12 12:26:40 +0400

    Codes

---
+++ Client Side/js/script.js
@@ -0,0 +1,8 @@
+var FName = "Rasim";
+console.log(FName);
+console.log(typeof(FName));
+
+// let LName = "Abbasov";
+// const Pi = 3.14;
+FName = 15;
+console.log(typeof(FName));
---
+++ DataTypes/DataTypes/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //byte age = 25;
+            //float Pi = 3.14F;
+            //double sum = 3562.25;
+            //decimal range = 451365.325M;
+            //char at = '@';
+            //bool isMarried = false;
+            //string Name = "Rasim ";//6*2=12
+
+            int a = 15;
+            a = a + 1;//a+=1;a++
+            int b = 4;
+            b = b + 5;//b+=5;
+            //a++;
+            //a--;
+            //Console.WriteLine(++a);
+            //Console.WriteLine(a != b);
+
+            Console.WriteLine(a < 30 && (b < 3 || true));
+        }
+    }
+}
